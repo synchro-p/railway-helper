0617de187c1632fb3761771afc617e1e5d576c89
Petr <p.samoseiko@g.nsu.ru>
2022-12-01 14:24:49 +0700

    added emergency classes

--- Software_p1/src/main/java/nsu/fit/railway/entities/event/TrackEmergency.java
+++ Software_p1/src/main/java/nsu/fit/railway/entities/event/TrackEmergency.java
@@ -1,4 +1,16 @@
 package nsu.fit.railway.entities.event;
 
+import nsu.fit.railway.entities.topology.Track;
+
 public class TrackEmergency {
+
+    private final Track brokenTrack;
+
+    public TrackEmergency(Track track) {
+        brokenTrack = track;
+    }
+
+    public Track getBrokenTrack() {
+        return brokenTrack;
+    }
 }
