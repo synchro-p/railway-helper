386fa5241685c190e7ceedb188bbf0143c05fe56
Petr <p.samoseiko@g.nsu.ru>
2022-12-27 16:30:42 +0700

    suited ViewController to work with GUI class

--- Software_p1/src/main/java/nsu/fit/railway/control/ViewController.java
+++ Software_p1/src/main/java/nsu/fit/railway/control/ViewController.java
@@ -3,6 +3,7 @@ package nsu.fit.railway.control;
 import nsu.fit.railway.entities.event.EventQueue;
 import nsu.fit.railway.entities.timetable.Timetable;
 import nsu.fit.railway.entities.topology.Topology;
+import nsu.fit.railway.gui.Main;
 
 import java.time.LocalDateTime;
 import java.util.AbstractMap.SimpleEntry;
@@ -12,21 +13,31 @@ public class ViewController {
     private final Topology topology;
     private final Timetable timetable;
 
-    public ViewController(Topology topology, Timetable timetable) {
+    private final Main gui;
+
+    private EventQueue events;
+    private EventProcessor processor;
+    private EmergencyConfiguration configuration;
+    public ViewController(Topology topology, Timetable timetable, Main gui) {
         this.topology = topology;
         this.timetable = timetable;
+        this.gui = gui;
     }
 
     public void startSimulation() {
-        EventQueue events = new Planner().createSchedule(topology,timetable);
-        EmergencyConfiguration configuration = new EmergencyConfiguration();
-        EventProcessor processor = new EventProcessor(
+        this.events = new Planner().createSchedule(topology,timetable);
+        this.configuration = new EmergencyConfiguration();
+        this.processor = new EventProcessor(
                 new EmergencyGenerator(
                         configuration.getFrequency(), configuration.getPossibleTypes()),
                 events);
-        while(!events.isEmpty()) {
-            SimpleEntry<LocalDateTime, LocalDateTime> timestamp = processor.processEvent(events.poll());
-//            SimulationGUI.changeView(Topology, Datetime);
+    }
+
+    public void simulationStep() {
+        if (events.peek() == null) {
+//            gui.tellUserNoMoreEvents();
         }
+            SimpleEntry<LocalDateTime, LocalDateTime> timestamp = processor.processEvent(events.poll());
+//            gui.changeView(topology, timestamp);
     }
 }
